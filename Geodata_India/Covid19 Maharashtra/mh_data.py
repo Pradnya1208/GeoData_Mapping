7f308b526d496c06149b95c3066acc9db2e0ab72
Pradnya1208 <pradnyapatil671@gmail.com>
2020-08-06 07:38:24 +0530

    Covid19 CSV for Recovered and Deceased cases in Maharashtra

--- Geodata_India/Covid19 Maharashtra/mh_data.py
+++ Geodata_India/Covid19 Maharashtra/mh_data.py
@@ -8,7 +8,10 @@ JSON_URL = 'https://api.covid19india.org/v5/min/timeseries-MH.min.json'
 dist_name  = []
 colNames = ['Districts,']
 covidData =[]
-dateCol =[] 
+covidRec = []
+covidDeath = []
+dateCol =[]
+keys = [] 
 #df = pd.read_json(JSON_URL)
 req = requests.get(JSON_URL)
 
@@ -33,7 +36,12 @@ for dist  in dist_name:
     if not 'Unknown' in dist:
         i=0
         covidData.append('\n')
+        covidRec.append('\n')
         covidData.append(dist + ",")
+        covidRec.append(dist + ",")
+        covidDeath.append('\n')
+        covidDeath.append(dist + ",")
+        
         covid = df(req.json()['MH']['districts'][dist])
         
         
@@ -44,10 +52,12 @@ for dist  in dist_name:
                
             if i != dateCol.index(dates)+1:
          
-                print(str(i) + ":" + str(dateCol.index(dates)+1))
+                #print(str(i) + ":" + str(dateCol.index(dates)+1))
                 i = dateCol.index(dates)+1
                 for x in range (dateCol.index(dates)):    
                     covidData.append("0,")
+                    covidRec.append("0,")
+                    covidDeath.append("0,")
                 
         # in case of fixed Dates
         #if  len(covid.index) != len(colNames)-1:
@@ -58,8 +68,25 @@ for dist  in dist_name:
          
                 
         for conf in covid.dates:
-            covidData.append(str(conf['total']['confirmed']) + ",")
-            #print(dist  + " : " + str(d['total']['confirmed']))
+            for t in conf.keys():
+                if 'total' in t:
+                    if 'confirmed' in (conf['total'].keys()):
+                        covidData.append(str(conf['total']['confirmed']) + ",")
+                    if not 'confirmed' in (conf['total'].keys()):
+                        covidRec.append("0,")
+                        
+                    if 'recovered' in (conf['total'].keys()):
+                        covidRec.append(str(conf['total']['recovered']) + ",")
+                    if not 'recovered' in (conf['total'].keys()):
+                        covidRec.append("0,")
+                        
+                    if 'deceased' in (conf['total'].keys()):
+                        covidDeath.append(str(conf['total']['deceased']) + ",")
+                    if not 'deceased' in (conf['total'].keys()):
+                        covidDeath.append("0,")
+                        
+# TODO: Delta and no of tests
+                                
                       
 
 for elem in dateCol:
@@ -68,7 +95,14 @@ for elem in dateCol:
 with open('Maharashtra_total_confirmed_cases.csv', 'w') as f:
     f.writelines(colNames)
     f.writelines(covidData)
+    
+with open('Maharashtra_total_recovered_cases.csv', 'w') as f:
+    f.writelines(colNames)
+    f.writelines(covidRec)
 
+with open('Maharashtra_total_Death_toll.csv', 'w') as f:
+    f.writelines(colNames)
+    f.writelines(covidDeath)
 
 
     
